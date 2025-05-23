1fbc2e65c859080a1945530ff8b7479136a29905
FaaizHaikal <faaizhaikal@gmail.com>
2025-05-24 01:33:12 +0700

    feat: add to cart from product page test

--- src/test/java/com/periplus/CartTest.java
+++ src/test/java/com/periplus/CartTest.java
@@ -10,6 +10,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.periplus.pages.LoginPage;
+import com.periplus.pages.ProductPage;
 import com.periplus.pages.HomePage;
 import com.periplus.pages.CheckoutPage;
 import com.periplus.utils.ConfigReader;
@@ -20,22 +21,34 @@ public class CartTest
   private LoginPage loginPage;
   private HomePage homePage;
   private CheckoutPage checkoutPage;
+  private ProductPage productPage;
 
   @BeforeMethod
   public void setup() {
     driver = new ChromeDriver();
+    driver.manage().window().maximize();
 
 		loginPage = new LoginPage(driver);
     homePage = new HomePage(driver);
     checkoutPage = new CheckoutPage(driver);
+    productPage = new ProductPage(driver);
 
     loginPage.login(ConfigReader.getProperty("periplus.email"), ConfigReader.getProperty("periplus.password"));
   }
 
-  @Test
-  public void addToCartTest()
-  {
+  @Test (priority =  1)
+  public void homePageAddToCartTest() {
     String bookId = homePage.addRandomProductToCart();
+
+    assertTrue(checkoutPage.bookInCart(bookId));
+  }
+
+  @Test (priority = 2)
+  public void productPageAddToCartTest() {
+    String bookUrl = homePage.selectRandomBookUrl();
+
+    productPage.openPage(bookUrl);
+    String bookId = productPage.addToCart();
 
     assertTrue(checkoutPage.bookInCart(bookId));
   }
@@ -43,7 +56,7 @@ public class CartTest
   @AfterMethod
   public void tearDown() {
     if (driver != null) {
-      // driver.quit();
+      driver.quit();
     }
   }
 }

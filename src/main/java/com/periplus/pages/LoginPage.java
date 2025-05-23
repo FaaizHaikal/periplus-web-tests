20bf72d3e58d41d6fd3b94362816c0254caec655
FaaizHaikal <faaizhaikal@gmail.com>
2025-05-23 11:46:37 +0700

    feat: add login page

---
+++ src/main/java/com/periplus/pages/LoginPage.java
@@ -0,0 +1,30 @@
+package com.periplus.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage {
+  @FindBy(name = "email")
+  private WebElement emailField;
+
+  @FindBy(id = "ps")
+  private WebElement passwordField;
+
+  @FindBy(id = "button-login")
+  private WebElement loginButton;
+  
+  public LoginPage(WebDriver driver) {
+    super(driver);
+  }
+
+  public void login(String email, String password) {
+    emailField.clear();
+    emailField.sendKeys(email);
+
+    passwordField.clear();
+    passwordField.sendKeys(password);
+
+    loginButton.click();
+  }
+}

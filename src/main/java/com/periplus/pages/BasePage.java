d10cd74691b10536824cd76f1721bb0bc5dc68d4
FaaizHaikal <faaizhaikal@gmail.com>
2025-05-23 20:11:37 +0700

    feat: add page url

--- src/main/java/com/periplus/pages/BasePage.java
+++ src/main/java/com/periplus/pages/BasePage.java
@@ -1,11 +1,24 @@
 package com.periplus.pages;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public abstract class BasePage {
   protected WebDriver driver;
+  protected WebDriverWait driverWait;
+  protected String pageUrl;
+  static final String PERIPLUS_URL = "https://www.periplus.com/";
 
   public BasePage(WebDriver driver) {
     this.driver = driver;
+    this.driverWait = new WebDriverWait(driver, Duration.ofSeconds(5));
+    PageFactory.initElements(driver, this);
+  }
+
+  public String getPageUrl() {
+    return this.pageUrl;
   }
 }

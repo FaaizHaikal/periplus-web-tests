de642cbf544fe1c4b1f75b64b583528fe7ffdc4a
FaaizHaikal <faaizhaikal@gmail.com>
2025-05-23 23:31:53 +0700

    feat: add to cart test

--- src/test/java/com/periplus/CartTest.java
+++ src/test/java/com/periplus/CartTest.java
@@ -1,34 +1,49 @@
 package com.periplus;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.periplus.pages.LoginPage;
+import com.periplus.pages.HomePage;
+import com.periplus.pages.CheckoutPage;
 import com.periplus.utils.ConfigReader;
 
 public class CartTest 
 {
   private WebDriver driver;
   private LoginPage loginPage;
+  private HomePage homePage;
+  private CheckoutPage checkoutPage;
 
   @BeforeMethod
   public void setup() {
     driver = new ChromeDriver();
-		driver.manage().window().maximize();
 
 		loginPage = new LoginPage(driver);
+    homePage = new HomePage(driver);
+    checkoutPage = new CheckoutPage(driver);
+
+    loginPage.login(ConfigReader.getProperty("periplus.email"), ConfigReader.getProperty("periplus.password"));
   }
 
   @Test
-  public void loginTest()
+  public void addToCartTest()
   {
-    loginPage.login(ConfigReader.getProperty("periplus.email"), ConfigReader.getProperty("periplus.password"));
+    String bookId = homePage.addRandomProductToCart();
 
-    String currentUrl = driver.getCurrentUrl();
-    assertFalse(currentUrl == loginPage.getPageUrl());
+    assertTrue(checkoutPage.bookInCart(bookId));
+  }
+
+  @AfterMethod
+  public void tearDown() {
+    if (driver != null) {
+      // driver.quit();
+    }
   }
 }
